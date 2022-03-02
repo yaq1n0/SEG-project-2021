d59d32507fae54da451d0ab9448df3e6fa6fd637
James Harcourt <jh3g20@soton.ac.uk>
2022-03-02 20:01:05 +0000

    Top-Down, Side-On Selection
    Bound BooleanProperty and attached event methods to menu buttons in FXML.

--- src/main/java/uk/ac/soton/comp2211/app/MainController.java
+++ src/main/java/uk/ac/soton/comp2211/app/MainController.java
@@ -1,6 +1,8 @@
 package uk.ac.soton.comp2211.app;
 
 import javafx.application.Platform;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.GraphicsContext;
@@ -26,12 +28,20 @@ public class MainController implements Initializable {
     private Button testButton;
     
     private RunwayView runwayView;
-            
+    private final BooleanProperty topView;
+
+    public MainController() {
+        this.topView = new SimpleBooleanProperty(true);
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         this.runwayView = new RunwayView();
         this.runwayContainer.getChildren().add(this.runwayView);
-        this.testButton.setOnAction(this::doSomething);
+        this.testButton.setOnAction(this.runwayView::update);
+        
+        // Bind the boolean properties to show which profile the runway view should be.
+        this.runwayView.bindViewProperty(this.topView);
     }
 
     private void doSomething(ActionEvent actionEvent) {
@@ -40,5 +50,23 @@ public class MainController implements Initializable {
             gc.setFill(Color.RED);
             gc.fillRect(0, 60,0, 80);
         });
+    }
+
+    /**
+     * Ran when user selects Top-Down in Menu>View
+     * @param actionEvent event
+     */
+    @FXML
+    private void selectTopView(ActionEvent actionEvent) {
+        this.topView.set(true);
+    }
+
+    /**
+     * Ran when user selects Side-On in Menu>View
+     * @param actionEvent event
+     */
+    @FXML
+    private void selectSideView(ActionEvent actionEvent) {
+        this.topView.set(false);
     }
 }
--- src/main/java/uk/ac/soton/comp2211/component/RunwayView.java
+++ src/main/java/uk/ac/soton/comp2211/component/RunwayView.java
@@ -1,22 +1,40 @@
 package uk.ac.soton.comp2211.component;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
+
+import javafx.event.ActionEvent;
 
 /**
  * Custom component to draw runway visualisation from parameters.
  */
 public class RunwayView extends Canvas {
     
-    private int length;
+    // private Runway runway;
+    private final BooleanProperty topView;
     
     public RunwayView() {
         super(600, 400);
+        topView = new SimpleBooleanProperty(true);
     }
-    
-    public void update() {
-        
+
+    /**
+     * Bind view property from controller. True:Top, False:Side
+     */
+    public void bindViewProperty (BooleanProperty viewProperty) { 
+        this.topView.bind(viewProperty);
+    }
+
+    /**
+     * Redraw the runway in the correct view with current parameters.
+     */
+    public void update(ActionEvent actionEvent) {
+        if (this.topView.get()) {
+            System.out.println("Draw Top Down");
+        } else {
+            System.out.println("Draw Side On");
+        }
     }
     
     
