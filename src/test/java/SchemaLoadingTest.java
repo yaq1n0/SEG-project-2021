1d38c1fa98ff7b60f4ba95269c44d2fc9c34bb0a
JamesHarcourt7 <jh3g20@soton.ac.uk>
2022-03-14 18:16:57 +0000

    hnnngg loadSchema redundant

--- src/test/java/SchemaLoadingTest.java
+++ src/test/java/SchemaLoadingTest.java
@@ -6,49 +6,41 @@ public class SchemaLoadingTest {
 
     @Test
     void testAirportFileCorrectFormat() {
-        SystemModel.loadSchemas();
         Assertions.assertDoesNotThrow(() -> {SystemModel.loadAirport("heathrow-correct.xml");});
     }
 
     @Test
     void testAirportFileWrongFormat() {
-        SystemModel.loadSchemas();
         Assertions.assertThrows(Exception.class, () -> {SystemModel.loadAirport("heathrow-broken.xml");});
     }
 
     @Test
     void testAirportFileNegativeRunwayValue() {
-        SystemModel.loadSchemas();
         Assertions.assertThrows(Exception.class, () -> {SystemModel.loadAirport("airport-brokenNegativeValue.xml");});
     }
 
     @Test
     void testAirportFileInvalidRunwayNumPerTarmac() {
-        SystemModel.loadSchemas();
         Assertions.assertThrows(Exception.class, () -> {SystemModel.loadAirport("airport-brokenInvalidNumRunways.xml");});
     }
 
     @Test
     void testAirportFileCorrectFormat2() {
-        SystemModel.loadSchemas();
         Assertions.assertDoesNotThrow(() -> {SystemModel.loadAirport("gatwick.xml");});
     }
 
     @Test
     void testAirportFileCorrectFormat3() {
-        SystemModel.loadSchemas();
         Assertions.assertDoesNotThrow(() -> {SystemModel.loadAirport("manchester.xml");});
     }
 
     @Test
     void testAirportFileCorrectFormat4() {
-        SystemModel.loadSchemas();
         Assertions.assertDoesNotThrow(() -> {SystemModel.loadAirport("edinburgh.xml");});
     }
 
     @Test
     void testObstaclesFileCorrectFormat() {
-        SystemModel.loadSchemas();
         Assertions.assertDoesNotThrow(SystemModel::loadObstacles);
     }
 
